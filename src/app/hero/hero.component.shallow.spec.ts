a57e28012a264ded321d53b667d4f5fc57ae7f1c
Tore Aurstad <tore.aurstad@gmail.com>
2019-08-01 18:08:28 +0200

    More work on module 4

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,27 @@
+import { HeroComponent } from './hero.component';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('HeroComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(HeroComponent);
+  });
+
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3};
+    expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+  });
+
+  it('should render the name inside an anchor tag', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3};
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+  });
+
+});
